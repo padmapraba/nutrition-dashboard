e8ec08b76eeaa246f5a8b3401d127983e73535f6
Padma Prabagaran <40726850+padmapraba@users.noreply.github.com>
2022-04-21 12:15:16 -0700

    no more color issues

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var svgA, svgB, svgC, svgD, svgE, svgD1;
 var width, height, innerHeight, innerWidth;
 var margin = { top: 50, right: 60, bottom: 60, left: 100 };
 var ingredients_data, descrip_data, attrFoodType, attrFoodItem, attrNutrient;
-var fact;
+var fact, colorScale;
 var xScale, yScale, plot, back;
 
 
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // data wrangling
 
-    drawA();
+    drawC();
   });
 });
 
@@ -108,7 +108,7 @@ function drawC() {
   attrFoodType = d3.select("#type").property("value");
   attrNutrient = d3.select("#nutr").property("value");
   attrFoodItem = d3.select("#item").property("value");
-  attrFoodItem = "blue";
+  // attrFoodItem = "blue";
 
   var widthC = svgC.node().clientWidth;
   var heightC = svgC.node().clientHeight;
@@ -128,132 +128,42 @@ function drawC() {
   });
 
   console.log(foodlist);
+    colorScale = {
+      'Protein':"#bf045b",
+      'Carbohydrate':"#bf5e04",
+      'Lipids':"#0464b3",
+      "Monosaturated Fats":"#0464b3",
+      "Polysaturated Fats":"#0464b3",
+      "Saturated Fats":"#0464b3",
 
-  var color = d3
-    .scaleOrdinal()
-    .domain(foodlist)
-    .range([
-      "#1b70fc",
-      "#faff16",
-      "#d50527",
-      "#158940",
-      "#f898fd",
-      "#24c9d7",
-      "#cb9b64",
-      "#866888",
-      "#22e67a",
-      "#e509ae",
-      "#9dabfa",
-      "#437e8a",
-      "#b21bff",
-      "#ff7b91",
-      "#94aa05",
-      "#ac5906",
-      "#82a68d",
-      "#fe6616",
-      "#7a7352",
-      "#f9bc0f",
-      "#b65d66",
-      "#07a2e6",
-      "#c091ae",
-      "#8a91a7",
-      "#88fc07",
-      "#ea42fe",
-      "#9e8010",
-      "#10b437",
-      "#c281fe",
-      "#f92b75",
-      "#07c99d",
-      "#a946aa",
-      "#bfd544",
-      "#16977e",
-      "#ff6ac8",
-      "#a88178",
-      "#5776a9",
-      "#678007",
-      "#fa9316",
-      "#85c070",
-      "#6aa2a9",
-      "#989e5d",
-      "#fe9169",
-      "#cd714a",
-      "#6ed014",
-      "#c5639c",
-      "#c23271",
-      "#698ffc",
-      "#678275",
-      "#c5a121",
-      "#a978ba",
-      "#ee534e",
-      "#d24506",
-      "#59c3fa",
-      "#ca7b0a",
-      "#6f7385",
-      "#9a634a",
-      "#48aa6f",
-      "#ad9ad0",
-      "#d7908c",
-      "#6a8a53",
-      "#8c46fc",
-      "#8f5ab8",
-      "#fd1105",
-      "#7ea7cf",
-      "#d77cd1",
-      "#a9804b",
-      "#0688b4",
-      "#6a9f3e",
-      "#ee8fba",
-      "#a67389",
-      "#9e8cfe",
-      "#bd443c",
-      "#6d63ff",
-      "#d110d5",
-      "#798cc3",
-      "#df5f83",
-      "#b1b853",
-      "#bb59d8",
-      "#1d960c",
-      "#867ba8",
-      "#18acc9",
-      "#25b3a7",
-      "#f3db1d",
-      "#938c6d",
-      "#936a24",
-      "#a964fb",
-      "#92e460",
-      "#a05787",
-      "#9c87a0",
-      "#20c773",
-      "#8b696d",
-      "#78762d",
-      "#e154c6",
-      "#40835f",
-      "#d73656",
-      "#1afd5c",
-      "#c4f546",
-      "#3d88d8",
-      "#bd3896",
-      "#1397a3",
-      "#f940a5",
-      "#66aeff",
-      "#d097e7",
-      "#fe6ef9",
-      "#d86507",
-      "#8b900a",
-      "#d47270",
-      "#e8ac48",
-      "#cf7c97",
-      "#cebb11",
-      "#718a90",
-      "#e78139",
-      "#ff7463",
-      "#bea1fd",
-    ]);
+      'Zinc':"#b30f04",
+      'Sodium':"#b30f04",
+      "Calcium":"#b30f04",
+      "Copper":"#b30f04",
+      "Iron":"#b30f04",
+      "Magnesium":"#b30f04",
+      "Phosphorus":"#b30f04",
+      "Potassium":"#b30f04",
+
+      "Vitamin A - RAE":"#8404b3",
+      "Vitamin B12":"#8404b3",
+      "Vitamin B6":"#8404b3",
+      "Vitamin C":"#8404b3",
+      "Vitamin E":"#8404b3",
+      "Vitamin K":"#8404b3",
+      
+      'Alpha Carotene':'salmon',
+      'Beta Carotene':'orange red',
+      'Cholesterol': 'teal',
+      'Fiber':'plum',
+      'Sugar Total':'#6495ED'
+      
+    };
 
   let max = d3.max(foodData, (d) => +d[attrNutrient]);
   let min = d3.min(foodData, (d) => +d[attrNutrient]);
 
-  console.log(min, max);
+  // console.log(min, max);
 
   var size = d3.scaleLinear().domain([min, max]).range([7, 35]);
 
@@ -281,7 +191,8 @@ function drawC() {
     .attr("r", (d) => size(+d[attrNutrient]))
     .attr("cx", widthC / 2)
     .attr("cy", heightC / 2)
-    .style("fill", (d) => color(d.Description))
+    // .style("fill", colorScale[attrNutrient])
+    .style('fill', d => fillCirc(d))
     .style("fill-opacity", 0.9)
     .style("stroke-width", 1)
     .on("mouseover", function (d, i) {
@@ -368,7 +279,19 @@ function drawC() {
     .attr('fill', 'black')
     .style("font-weight", "bold")
     .text(attrNutrient);
+
+    function fillCirc(d){
+      console.log(attrFoodItem);
+      if(d.Description == attrFoodItem){
+        return '#808080';
+      }else{
+        return colorScale[attrNutrient];
+      }
+    }
+  
 }
+
+
 
 function drawB() {
   const margin = { top: 50, bottom: 50, left: 70, right: 50 };
